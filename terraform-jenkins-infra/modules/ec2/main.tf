d362c486c00509ffaf26c141cd96ca208b247fc4
Shubham Juyal <shubhamjuyal4444@gmail.com>
2024-09-30 23:17:23 +0530

    load balance shi

--- terraform-jenkins-infra/modules/ec2/main.tf
+++ terraform-jenkins-infra/modules/ec2/main.tf
@@ -9,9 +9,9 @@ variable "user_data_install_jenkins" {}
 
 
 resource "aws_instance" "ec2_jenkins" {
-  ami                         = ami_id
-  instance_type               = instance_type
-  subnet_id                   = var.subnet_id
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id[0]
   vpc_security_group_ids      = var.sg_for_jenkins
   associate_public_ip_address = var.enable_public_ip_address
   user_data                   = var.user_data_install_jenkins
@@ -21,8 +21,13 @@ resource "aws_instance" "ec2_jenkins" {
     http_tokens   = "required"
   }
 
-  key_name = "aws_for terraform"
+  key_name = "aws_for_terraform"
   tags = {
     Name = var.tag_name
   }
+}
+
+resource "aws_key_pair" "public_key_for_jenkins_instance" {
+  key_name   = "aws_for_terraform"
+  public_key = var.public_key
 }
